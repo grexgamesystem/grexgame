28e9f8ad77d7d220065dcbb332ba1d2f7f5c4e16
Luis del Río <luis@ioioio.mx>
2016-03-23 01:05:46 -0600

    integration of navigator

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var config = require('./config');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,7 +10,9 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
 app.get('/', function(request, response) {
+
   response.render('pages/index');
 });
 
