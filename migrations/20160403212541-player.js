14376ae75ae183f40d6190b614489b3257d8f923
Luis del Río <luis@ioioio.mx>
2016-04-05 22:04:25 -0500

    game db mode

--- migrations/20160403212541-player.js
+++ migrations/20160403212541-player.js
@@ -2,19 +2,25 @@ var dbm = global.dbm || require('db-migrate');
 var type = dbm.dataType;
 
 exports.up = function(db, callback) {
-  db.createTable('location', {
+  db.createTable('player', {
     id: { type: 'serial', primaryKey: true, autoIncremet: true },
+    location_id: { type: 'int', notNull: true, foreignKey: {
+    	name: 'player_has_many_locations',
+            table: 'location',
+            rules: {
+                onDelete: 'CASCADE',
+                onUpdate: 'RESTRICT'
+            },
+            mapping: 'id'
+    } },
     name: 'string',
-    lat: 'float(24)',
-    long: 'float(24)',
-    is_active: 'boolean',
+    food_found: 'int',
     created_at: { type: 'timestamp', defaultValue: 'now' },
     updated_at: 'timestamp',
   }, callback);
+
 };
 
 exports.down = function(db, callback) {
-  db.dropTable('games', function(err) {
-    if (err) { callback(err); return; }
-  },callback);
+  callback();
 };
