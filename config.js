d89eb8de4bd73e3c67b3984b3b1168cd79fc07ea
Luis del Río <luis@ioioio.mx>
2016-04-05 22:39:37 -0500

    db config

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ var dbName = process.env.DB_NAME || 'grex'
 var dbUser = process.env.DB_USER || 'root'
 var dbPass = process.env.DB_PASSWORD || 'root'
 
-var conString = "postgres://"+dbUser+":"+dbPass+"@"+dbHost+":5432/"+dbName;
+var conString = process.env.DATABASE_URL ? process.env.DATABASE_URL : "postgres://"+dbUser+":"+dbPass+"@"+dbHost+":5432/"+dbName;
 
 exports.db = orm.connect(conString, function (err, db) {
   if (err) throw err;
